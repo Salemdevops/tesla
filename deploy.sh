68847603f94f5fdabc93fdfb8929e8fb19e53031
Simon <simon@gmail.com>
2022-05-25 13:27:39 -0400

    conflict 2 resolved

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@ deploying java application
 Deploy python apps
 deploy in the dev env
 deployment5 master
-deploymen4
-
-
-
-
-
+deployment4
+deploy to staging env
+echo good job.
+Tested and accepted
